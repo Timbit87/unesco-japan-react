9474a35a654ccd44efba2e44b14068a56bcb244a
Andrew Beckett <Andrewbeckett95@gmail.com>
2024-10-10 09:45:28 +0900

    Initial groundwork for object laid

---
+++ src/components/unesco_object.js
@@ -0,0 +1,15 @@
+import React from 'react';
+
+  const UnescoObject = [
+    {
+      name: 'Todai-ji',
+      prefecture: 'Nara',
+      latitude: '3',
+      longitude: '3',
+      picture: './images/toadai-ji.jpg',
+      dateAdded: 'September 11th 2001',
+      shortDescription: 'Todai-ji is a temple located in Nara. Get fucked.'
+    }
+  ]
+
+export default UnescoObject
--- src/index.js
+++ src/index.js
@@ -3,12 +3,11 @@ import ReactDOM from 'react-dom/client';
 import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
+import UnescoCard from './components/unesco_card'
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>
+    <UnescoCard/>
 );
 
 // If you want to start measuring performance in your app, pass a function
