e3cd2e141b25fc702f7f6392dbd548dca34e76ae
Andrew Beckett <andrewbeckett95@gmail.com>
2024-10-14 01:06:51 +0900

    yeah

--- src/components/map.js
+++ src/components/map.js
@@ -10,7 +10,7 @@ const Map = () => {
   const mapRef = useRef()
   const mapContainerRef = useRef()
   useEffect(() => {
-    mapboxgl.accessToken = 'pk.eyJ1IjoidGltYml0ODciLCJhIjoiY20xazBuazFzMTk5OTJ2c2FoYjBhOWxjYSJ9.nMG02Ci2jFRPBGoSi9F-Sg'
+    mapboxgl.accessToken = 'no'
     mapRef.current = new mapboxgl.Map({
       container: mapContainerRef.current,
       style: 'mapbox://styles/mapbox/streets-v11',
