b1fff7f07211bf069de9f51b65c921983f283908
Andrew Beckett <andrewbeckett95@gmail.com>
2024-10-25 22:08:44 +0900

    Added all the way down to the Meiji Sites

--- src/components/unesco_object.js
+++ src/components/unesco_object.js
@@ -20,7 +20,7 @@ import munakata from '../images/munakata.jpg'
 import nachiWaterfall from '../images/nachi-waterfall.jpg'
 import nagasakiGod from '../images/nagasaki-god.jpg'
 import ogasawara from '../images/ogasawara.jpg'
-import okinawa from '../images/Okinawa.jpg'
+import shinkinaen from '../images/Okinawa.jpg'
 import tombs from '../images/tombs.jpg'
 import tomiokaSilk from '../images/tomioka-silk.jpg'
 
@@ -46,7 +46,7 @@ import tomiokaSilk from '../images/tomioka-silk.jpg'
       name: 'Shirakawa-go and Gokayama',
       prefecture: 'Gifu and Toyama',
       coordinates: [
-        { title: 'Suganuma Gassho Village', latitude: 36.40423, longitude: 136.88645 },
+        { title: 'Suganuma Gassho Village', latitude: 36.40423, longitude: 136.88645, img: shirakawagoImage },
         { title: 'Shirakawa-Go', latitude: 36.25797, longitude: 136.90626 },
         { title: 'Ainokura Gassho Village', latitude: 36.42603, longitude: 136.93548 }
       ],
@@ -57,7 +57,7 @@ import tomiokaSilk from '../images/tomioka-silk.jpg'
     {
       name: 'Atomic Bomb Dome',
       prefecture: 'Hiroshima',
-      coordinates: [ {title: 'Atomic Bomb Dome', latitude: 34.395556, longitude: 132.453611} ],
+      coordinates: [ {title: 'Atomic Bomb Dome', latitude: 34.395556, longitude: 132.453611, img: bombDomeImage }],
       cardImg: bombDomeImage,
       dateAdded: '1996',
       shortDescription: 'The bomb dome is what happens when you become friends with Joel'
@@ -65,7 +65,7 @@ import tomiokaSilk from '../images/tomioka-silk.jpg'
     {
       name: "Itsukushima Jinja",
       prefecture: "Hiroshima",
-      coordinates: [ {title: 'Itsukushima Jinja', latitude: 34.295833, longitude: 132.319722} ],
+      coordinates: [ {title: 'Itsukushima Jinja', latitude: 34.295833, longitude: 132.319722, img: itsukushima }],
       cardImg: itsukushima,
       dateAdded: '1996',
       shortDescription: 'Itsukushima Jinja is amazing. Unlike Joel.'
@@ -73,7 +73,7 @@ import tomiokaSilk from '../images/tomioka-silk.jpg'
     {
       name: 'Himeji Castle',
       prefecture: 'Hyogo',
-      coordinates: [{ title: 'Himeji Castle', latitude: 34.839444, longitude: 134.693889 }],
+      coordinates: [{ title: 'Himeji Castle', latitude: 34.839444, longitude: 134.693889, img: himejiCastle }],
       cardImg: himejiCastle,
       dateAdded: '1993',
       shortDescription: 'Himeji Castle is the most amazing castle still standing.'
@@ -81,7 +81,7 @@ import tomiokaSilk from '../images/tomioka-silk.jpg'
     {
       name: 'Yakushima Island',
       prefecture: 'Kagoshima',
-      coordinates: [{ title: 'Yakushima Island', latitude: 30.343889, longitude: 130.523889 }],
+      coordinates: [{ title: 'Yakushima Island', latitude: 30.343889, longitude: 130.523889, img: yakushima }],
       cardImg: yakushima,
       dateAdded: '1993',
       shortDescription: "Yakushima, not to be confused with Yakuzashima, is an island."
@@ -89,7 +89,7 @@ import tomiokaSilk from '../images/tomioka-silk.jpg'
     {
       name: 'Shirakami-Sanchi',
       prefecture: 'Aomori & Akita',
-      coordinates: [{ title: 'Shirakami-Sanchi', latitude: 40.47, longitude: 140.13 }],
+      coordinates: [{ title: 'Shirakami-Sanchi', latitude: 40.47, longitude: 140.13, img: shirakamiSanchi }],
       cardImg: shirakamiSanchi,
       dateAdded: '1993',
       shortDescription: 'Some mountains yo.'
@@ -99,7 +99,7 @@ import tomiokaSilk from '../images/tomioka-silk.jpg'
       prefecture: 'Tochigi',
       coordinates: [
         { title: "Futarasan Shrine", latitude: 36.75846, longitude: 139.5964 },
-        { title: "Nikko Toshogu", latitude: 36.75814, longitude: 139.5987 },
+        { title: "Nikko Toshogu", latitude: 36.75814, longitude: 139.5987, img: nikko },
         { title: "Rinno-Ji", latitude: 36.75449, longitude: 139.6007 }
       ],
       cardImg: nikko,
@@ -109,7 +109,7 @@ import tomiokaSilk from '../images/tomioka-silk.jpg'
     {
       name: 'Shiretoko National Park',
       prefecture: 'Hokkaido',
-      coordinates: [{ title: 'Shiretoko National Park', latitude: 44.1, longitude: 145.1833333 }],
+      coordinates: [{ title: 'Shiretoko National Park', latitude: 44.1, longitude: 145.1833333, img: shiretoko }],
       cardImg: shiretoko,
       dateAdded: '2005',
       shortDescription: 'Natural nature park in Hokkaido'
@@ -118,7 +118,7 @@ import tomiokaSilk from '../images/tomioka-silk.jpg'
       name: 'Buddhist Monuments in the Horyu-ji Area',
       prefecture: 'Nara',
       coordinates: [
-        { title: "Horuji Kondo", latitude: 34.61475, longitude: 135.7342 },
+        { title: "Horuji Kondo", latitude: 34.61475, longitude: 135.7342, img: horyuji },
         { title: "Hoki-Ji", latitude: 34.62295, longitude: 135.7462 }
       ],
       cardImg: horyuji,
@@ -136,7 +136,7 @@ import tomiokaSilk from '../images/tomioka-silk.jpg'
         { title: 'Enryaku-Ji', latitude: 35.07045, longitude: 135.840925 },
         { title: 'Daigo-Ji', latitude: 34.950992, longitude: 135.819586 },
         { title: 'Ninna-Ji', latitude: 35.031008, longitude: 135.713772 },
-        { title: 'Byodo-In', latitude: 34.889292, longitude: 135.807692 },
+        { title: 'Byodo-In', latitude: 34.889292, longitude: 135.807692, img: byodoin },
         { title: 'Ujigami Shrine', latitude: 34.891944, longitude: 135.811389 },
         { title: 'Kozan-Ji', latitude: 35.060108, longitude: 135.678569 },
         { title: 'Saiho-Ji', latitude: 34.991961, longitude: 135.683314 },
@@ -162,10 +162,10 @@ import tomiokaSilk from '../images/tomioka-silk.jpg'
         { title: 'Nakagusuku Castle', latitude: 26.283889, longitude: 127.801389 },
         { title: 'Tamaudun', latitude: 26.218333, longitude: 127.714722 },
         { title: 'Sonohyan-utaki', latitude: 26.218075, longitude: 127.717352 },
-        { title: 'Shikaina-en', latitude: 26.204444, longitude: 127.715278 },
+        { title: 'Shikaina-en', latitude: 26.204444, longitude: 127.715278, img: shinkinaen },
         { title: 'Sefa-utaki', latitude: 26.173333, longitude: 127.826667 },
       ],
-      cardImg: okinawa,
+      cardImg: shinkinaen,
       dateAdded: '2000',
       shortDescription: 'Okinawan stuff',
     },
@@ -183,7 +183,7 @@ import tomiokaSilk from '../images/tomioka-silk.jpg'
         { title: 'Kumano Hayatama Taisha', latitude: 33.731953, longitude: 135.983725 },
         { title: 'Kumano Nachi Taisha', latitude: 33.668611, longitude: 135.890556 },
         { title: 'Seiganto-ji', latitude: 33.6694, longitude: 135.8898 },
-        { title: 'Nachi Falls', latitude: 33.672, longitude: 135.891 },
+        { title: 'Nachi Falls', latitude: 33.672, longitude: 135.891, img: nachiWaterfall },
         { title: 'Nachi Primeval Forest', latitude: 33.675274, longitude: 135.886116 },
         { title: 'Fudarakusan-ji', latitude: 33.644725, longitude: 135.934436 },
         { title: 'Niutsuhime Shrine', latitude: 34.262722, longitude: 135.5195 },
@@ -201,7 +201,7 @@ import tomiokaSilk from '../images/tomioka-silk.jpg'
       name: 'Iwami Ginzan Silver Mine and its Cultural Landscape',
       prefecture: 'Shimane',
       coordinates: [
-        { title: 'Iwami Ginzan Silver Mine and its Cultural Landscape', latitude: 35.107222, longitude: 132.4375 }
+        { title: 'Iwami Ginzan Silver Mine and its Cultural Landscape', latitude: 35.107222, longitude: 132.4375, img: ginzan }
       ],
       cardImg: ginzan,
       dateAdded: '2007',
@@ -210,7 +210,12 @@ import tomiokaSilk from '../images/tomioka-silk.jpg'
     {
       name: 'Ogasawara Islands',
       prefecture: 'Tokyo',
-      coordinates: [{ title: 'Ogasawara Islands', latitude: 12, longitude: 12 }],
+      coordinates: [
+        { title: 'Chichijima', latitude: 27.0666667, longitude: 142.208333, },
+        { title: 'Hahajima', latitude: 26.65, longitude: 142.166667, },
+        { title: 'Iwo Jima', latitude: 24.780278, longitude: 141.324444, },
+        { title: 'Mukojima', latitude: 27.666667, longitude: 142.133056, },
+      ],
       cardImg: ogasawara,
       dateAdded: '2011',
       shortDescription: "I don't know why this was added."
@@ -232,7 +237,7 @@ import tomiokaSilk from '../images/tomioka-silk.jpg'
     {
       name: 'Fujisan, Sacred Place and Source of Artistic Inspiration',
       prefecture: 'Yamanashi and Shizuoka',
-      coordinates: [{ title: 'Fujisan, Sacred Place and Source of Artistic Inspiration', latitude: 35.360833, longitude: 138.7275 }],
+      coordinates: [{ title: 'Fujisan, Sacred Place and Source of Artistic Inspiration', latitude: 35.360833, longitude: 138.7275, img: fuji }],
       cardImg: fuji,
       dateAdded: '2013',
       shortDescription: "A nice looking triangle, that's for sure",
@@ -240,7 +245,7 @@ import tomiokaSilk from '../images/tomioka-silk.jpg'
     {
       name: 'Tomioka Silk Mill and Related Sites',
       prefecture: 'Gunma',
-      coordinates: [{ title: 'Tomioka Silk Mill', latitude: 36.255278, longitude: 138.887778 }],
+      coordinates: [{ title: 'Tomioka Silk Mill', latitude: 36.255278, longitude: 138.887778, img: tomiokaSilk }],
       cardImg: tomiokaSilk,
       dateAdded: '2014',
       shortDescription: 'A little worm told me how to make a gun. It fires worms'
@@ -248,7 +253,33 @@ import tomiokaSilk from '../images/tomioka-silk.jpg'
     {
       name: "Sites of Japan's Meiji Industrial Revolution",
       prefecture: 'Fukuoka, Saga, Nagasaki, Kumamoto, Kagoshima, Yamaguchi, Iwate, and Shizuoka',
-      coordinates: [{ title: 'Poop', latitude: 23, longitude: 23 }],
+      coordinates: [
+        { title: 'Hagi Reverberatory Furnace', latitude: 34.428153, longitude: 131.418297, },
+        { title: 'Ebisugahana Shipyard', latitude: 34.430028, longitude: 131.412111, },
+        { title: 'Ohitayama Tatara Iron Works', latitude: 34.506, longitude: 131.53825, },
+        { title: 'Hagi Castle', latitude: 34.421419, longitude: 131.381389, },
+        { title: 'Shokasonjuku Academy', latitude: 34.412154, longitude: 131.417342, },
+        { title: 'Former Shuseikan, Reverbatory Furnaces, Machine Factory, and Spinning Engineers Residence',
+          latitude: 31.617306, longitude: 130.57625, },
+        { title: 'Terayama Charcoal Kiln', latitude: 31.664278, longitude: 130.601102, },
+        { title: 'Sekiyoshi Sluice Gate of Yoshino Leat', latitude: 31.647083, longitude: 130.552194, },
+        { title: 'Nirayama Reverberatory Furnace', latitude: 35.040278, longitude: 138.961944, },
+        { title: 'Hashino Iron Mining and Smelting Site', latitude: 39.332778, longitude: 141.679722, },
+        { title: 'Mietsu Naval Dock', latitude: 33.2072222, longitude: 130.339722, },
+        { title: 'Kosuge Slip Dock', latitude: 32.726833, longitude: 129.862472, },
+        { title: 'Mitsubishi No. 3 Dry Dock', latitude: 32.742194, longitude: 129.858528, },
+        { title: 'Mitsubishi Senshokaku Guest House', latitude: 32.7383, longitude: 129.8569, },
+        { title: 'Mitsubishi Giant Cantilever Crane', latitude: 32.7422, longitude: 129.8592, },
+        { title: 'Mitsubishi Former Pattern Shop', latitude: 32.742069, longitude: 129.857053, },
+        { title: 'Takashima Coal Mine', latitude: 32.653889, longitude: 129.755389, },
+        { title: 'Hashima Coal Mine (Gunkanjima)', latitude: 32.6277778, longitude: 129.738333, img: gunkanjima },
+        { title: 'Glover House and Office', latitude: 32.733611, longitude: 129.869167, },
+        { title: 'Miike Coal Mine and Miike Port', latitude: 33.013611, longitude: 130.456111, },
+        { title: 'Misumi West Port', latitude: 32.62224089558422, longitude: 130.4556692254229, },
+        { title: 'The Imperial Steel Works: Office, Repairing Factory, and Metalsmith Factory',
+          latitude: 33.902778, longitude: 130.83, },
+        { title: 'Onga River Pumping Station', latitude: 33.811111, longitude: 130.706861, },
+      ],
       cardImg: gunkanjima,
       dateAdded: '2015',
       shortDescription: 'ROCK AND STONE',
@@ -256,7 +287,7 @@ import tomiokaSilk from '../images/tomioka-silk.jpg'
     {
       name: 'The Architectural Work of Le Corbusier',
       prefecture: 'Tokyo',
-      coordinates: [{ title: 'placer', latitude: 35.715278, longitude: 139.775833 }],
+      coordinates: [{ title: 'placer', latitude: 35.715278, longitude: 139.775833, img: leCorbusier }],
       cardImg: leCorbusier,
       dateAdded: '2016',
       shortDescription: 'A frog?'
